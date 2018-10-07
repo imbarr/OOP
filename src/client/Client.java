f93bd72c101407fb1bf063f51345f2cfabc05767
imbar <imbrkvskh@gmail.com>
2018-10-07 20:35:05 +0500

    Finished client and server

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,32 @@
 package client;
 
-public class Client {
-    public byte[] send(byte[] data) {
+import util.application_protocol.ApplicationProtocolException;
+import util.application_protocol.ProtocolMethods;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Client {
+    private Socket socket;
+    private InetAddress address;
+    private int port;
+
+    public Client(InetAddress address, int port) {
+        socket = new Socket();
+        this.address = address;
+        this.port = port;
+    }
+
+    public byte[] send(byte[] data) throws IOException, ApplicationProtocolException {
+        try(Socket s = socket; OutputStream os = s.getOutputStream(); InputStream is = s.getInputStream()) {
+            s.connect(new InetSocketAddress(address, port));
+            ProtocolMethods.writeAll(data, os);
+            return ProtocolMethods.readAll(is);
+        }
     }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -3,6 +3,7 @@ package server;
 import file_worker.executable.MD5Execution;
 import file_worker.FileWorker;
 import server.command_task.ServerThreadedTask;
+import server.server_task.ServerTask;
 import thread_dispatcher.ThreadDispatcher;
 
 import java.io.File;
@@ -12,22 +13,23 @@ import java.net.Socket;
 
 public class Server {
     private ThreadDispatcher dispatcher;
+    private ServerTask task;
     private volatile boolean stopped;
 
     public void stop() {
         stopped = true;
     }
 
-    public Server(File logging){
+    public Server(File logging, ServerTask task){
         dispatcher = ThreadDispatcher.getInstance(logging);
+        this.task = task;
     }
 
     public void start() throws IOException {
         try(ServerSocket server = new ServerSocket(40000)) {
             while (!stopped) {
                 Socket client = server.accept();
-                //TODO: Write ServerTask
-                dispatcher.add(new ServerThreadedTask(client, null));
+                dispatcher.add(new ServerThreadedTask(client, task));
             }
         }
     }
